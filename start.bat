a70539fb782cb39b083efe3fd9088739b09d5460
Cola_Colin <cola_colin@ki.tng.de>
2014-05-15 21:47:52 +0200

    missing bat files to make it easier on windows people who have never seen a shell in their life

---
+++ start.bat
@@ -0,0 +1,3 @@
+start node src\server.js
+timeout 3
+start explorer http://127.0.0.1:8080/ui/main/game/server_browser/server_browser.html
---
+++ startChromeApp.bat
@@ -0,0 +1,3 @@
+start node src\server.js
+timeout 3
+"C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" --app=http://127.0.0.1:8080/ui/main/game/server_browser/server_browser.html
